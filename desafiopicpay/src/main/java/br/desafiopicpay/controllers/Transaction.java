50fa02176b6f30030df526459e499ccc59bee3bd
Eliézer da Silva <eliezerdasilva2005@gmail.com>
2023-09-08 10:12:54 -0300

    :heavy_plus_sign: feat: create Trasaction entity

---
+++ desafiopicpay/src/main/java/br/desafiopicpay/controllers/Transaction.java
@@ -0,0 +1,42 @@
+package br.desafiopicpay.controllers;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import br.desafiopicpay.models.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(name="transactions")
+@Table(name="transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Transaction {
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Long id;
+	
+	private BigDecimal amount;
+	@JoinColumn(name="sender_id")
+	@ManyToOne
+	private User sender;
+	@JoinColumn(name="receiver_id")
+	@ManyToOne
+	private User receiver;
+	
+	private LocalDateTime timestamp;
+	
+
+}
